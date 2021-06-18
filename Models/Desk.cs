7a44bb919049380ddbf234dbcbbe6bf9655f86af
Yu-Chun Tung <dyj0183@byui.edu>
2021-06-18 15:48:02 -0600

    updated

--- Models/Desk.cs
+++ Models/Desk.cs
@@ -19,24 +19,50 @@ namespace RazorPagesMegaDesk
     public class Desk
     {
         // Constants
-        public const short MIN_WIDTH = 24;
-        public const short MAX_WIDTH = 96;
-        public const short MIN_DEPTH = 12;
-        public const short MAX_DEPTH = 48;
-        public const short MIN_DESK_DRAWERS = 0;
-        public const short MAX_DESK_DRAWERS = 7;
+        //public const short MIN_WIDTH = 24;
+        //public const short MAX_WIDTH = 96;
+        //public const short MIN_DEPTH = 12;
+        //public const short MAX_DEPTH = 48;
+        //public const short MIN_DESK_DRAWERS = 0;
+        //public const short MAX_DESK_DRAWERS = 7;
 
         // properties
         public int DeskId { get; set; }
+        [Range(24, 96)]
         public decimal Width { get; set; }
+        [Range(12, 48)]
         public decimal Depth { get; set; }
 
         [Display(Name = "Number Of Drawers")]
+        [Range(0, 7)]
         public decimal NumberOfDrawers { get; set; }
 
         public int DesktopMaterialId { get; set; }
 
         // navigation properties
         public DesktopMaterial SurfaceMaterial { get; set; }
+
+        //public decimal GetMaterialCost()
+        //{
+        //    switch (SurfaceMaterial.DesktopMaterialName)
+        //    {
+        //        case "Laminate":
+        //            return SurfaceMaterial.DesktopMaterialPrice;
+
+        //        case "Oak":
+        //            return OAK_COST;
+
+        //        case DesktopMaterial.Rosewood:
+        //            return ROSEWOOD_COST;
+
+        //        case DesktopMaterial.Veneer:
+        //            return VENEER_COST;
+
+        //        case DesktopMaterial.Pine:
+        //            return PINE_COST;
+        //        default:
+        //            return 0.00M;
+        //    }
+        //}
     }
 }
